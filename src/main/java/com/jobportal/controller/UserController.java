3c3d2f48da417b47f5d846a8cff40e8baecdded7
Vighnesh Tathare <v42069@yahoo.com>
2025-01-29 00:05:13 +0530

    add

---
+++ src/main/java/com/jobportal/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.jobportal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jobportal.entity.User;
+import com.jobportal.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("adduser")
+	public User addUser(@RequestBody User user) {
+		return userService.addUser(user);
+	}
+	
+	@GetMapping("/getusers")
+	public List<User> getUser() {
+		return userService.getUsers();
+	}
+}
